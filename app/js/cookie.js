de1435f4f506d02d13ac6a8ee02cc23bc3f03219
Luke Tanner <latanner@ucsc.edu>
2017-07-16 22:07:24 -0700

    initialized i in readCookie() for loop

--- app/js/cookie.js
+++ app/js/cookie.js
@@ -16,7 +16,7 @@ function createCookie(name, value, daysToExpire, path) {
 function readCookie(name) {
     var nameAttr = name + "=";
     var cookies = document.cookie.split(';');
-    for(var cookie=0;i < cookies.length;i++) {
+    for(var i = 0; i < cookies.length; i++) {
         var cookie = ca[i];
         while (cookie.charAt(0)==' ') cookie = cookie.substring(1,cookie.length);
         if (cookie.indexOf(nameAttr) == 0) return cookie.substring(nameAttr.length,cookie.length);
