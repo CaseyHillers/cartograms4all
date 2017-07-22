4f380a885eb9b0d49b22568cb7008b04ab5a0b51
Luke Tanner <latanner.ucsc.edu>
2017-07-22 11:51:10 -0700

    working on renaming .csv files serverside

--- app/js/functions.js
+++ app/js/functions.js
@@ -80,6 +80,9 @@ function saveCSV(userCSV) {
 
   var data = new FormData();
   data.append("input_csv", userCSV);
+  data.append("name", CSV)
+  console.log(CSV);
+  data.append("session_id", userSessionID);
 
   $.ajax({
     url: 'uploader/upload-manager.php',
--- app/uploader/upload-manager.php
+++ app/uploader/upload-manager.php
@@ -15,6 +15,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         } else{
             if (move_uploaded_file($_FILES["input_csv"]["tmp_name"], "upload/" . $_FILES["input_csv"]["name"])){
               echo "Your file was uploaded successfully.";
+
             } else {
                   echo "upload fail";
                   print_r($_FILES);
@@ -24,4 +25,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         die("Error: " . $_FILES["input_csv"]["error"]);
     }
 }
+
+function writeToFile(){
+  if(!empty($_POST['data'])){
+    $settings_string = $_POST['data'];
+    $session_file_name = "settings/";
+    $session_id = $_POST['name'];
+    $session_file_name .= ($session_id .= ".json");
+    file_put_contents($session_file_name, $settings_string);
+  }
+}
+
+function readFromFile($csvName){
+  $session_file_name = "upload/";
+  $session_id = $_POST['session_id'];
+  $session_file_name .= ($session_id .= ".json");
+  $session_file_string = file_get_contents($session_file_name);
+  echo ($session_file_string);
+}
 ?>
+
+
+
+
